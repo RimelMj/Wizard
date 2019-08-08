c59cd8ddfe2d1e008c29f52e72893fde838c026c
Rimel Morjane <rimel.morjane6@gmail.com>
2019-08-08 10:52:56 +0100

    Wizard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from "@angular/forms";
+import { FormGroup } from "@angular/forms";
+import { FormBuilder } from "@angular/forms";
+import { Validators } from "@angular/forms";
 
+
+export interface Food {
+   value: string;
+   display: string;
+}
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Wizard';
+export class AppComponent implements OnInit{
+   title = 'Wizard';   
+   isLinear=true;
+   firstFormGroup: FormGroup;
+   secondFormGroup: FormGroup;
+   thirdFormGroup: FormGroup;
+   emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,]);
+   
+   constructor(private _formBuilder: FormBuilder) {}
+   processFile(imageInput: any) {
+      const file: File = imageInput.files[0];
+      const reader = new FileReader();}
+   ngOnInit() {
+      this.firstFormGroup = this._formBuilder.group({
+         firstCtrl: ['', Validators.required]
+      });
+     
+      
+      this.thirdFormGroup = this._formBuilder.group({
+         thirdCtrl: ['', Validators.required]
+      });}
+     
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,100 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatProgressBarModule, MatTooltipModule, MatTabsModule, MatExpansionModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatFormFieldModule, MatButtonToggleModule, MatIconModule, MatProgressSpinnerModule, MatToolbarModule, MatSidenavModule, MatMenuModule, MatListModule, MatStepperModule } from '@angular/material';
+import { CdkStepperModule } from '@angular/cdk/stepper';
 
+import { A11yModule } from '@angular/cdk/a11y';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { PortalModule } from '@angular/cdk/portal';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
+import { CdkTableModule } from '@angular/cdk/table';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+
+import { MatGridListModule } from '@angular/material/grid-list';
+
+import { MatPaginatorModule } from '@angular/material/paginator';
+
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+
+import { MatTreeModule } from '@angular/material/tree';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule,
+    MatProgressBarModule,
+    MatExpansionModule,
+    BrowserAnimationsModule,
+    MatTooltipModule,
+    CdkStepperModule,
+    MatButtonModule, MatDatepickerModule, MatInputModule, MatNativeDateModule, MatFormFieldModule, FormsModule, ReactiveFormsModule, MatButtonToggleModule, MatIconModule, MatProgressSpinnerModule, MatToolbarModule, MatSidenavModule, MatMenuModule, MatListModule, MatStepperModule
+ 
+    ,A11yModule,
+    CdkStepperModule,
+    CdkTableModule,
+    CdkTreeModule,
+    DragDropModule,
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+  
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+    PortalModule,
+    ScrollingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
